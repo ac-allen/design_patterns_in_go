3754a93a92de3944ef27a1b3449f0df2cc8cee37
arvind chaudhary <arvindchaudhary@MacBook-Pro.local>
2023-08-31 10:38:55 +0530

    updated

--- 4.Factories/Prototype_Factory/creational.factories.protofactory.go
+++ 4.Factories/Prototype_Factory/creational.factories.protofactory.go
@@ -3,29 +3,33 @@ package factories
 import "fmt"
 
 type Employee struct {
-  Name, Position string
-  AnnualIncome int
+	Name, Position string
+	AnnualIncome   int
 }
 
 const (
-  Developer = iota
-  Manager
+	Developer = iota
+	Manager
 )
 
 // functional
 func NewEmployee(role int) *Employee {
-  switch role {
-  case Developer:
-    return &Employee{"", "Developer", 60000}
-  case Manager:
-    return &Employee{"", "Manager", 80000}
-  default:
-    panic("unsupported role")
-  }
+	switch role {
+	case Developer:
+		return &Employee{"", "Developer", 60000}
+	case Manager:
+		return &Employee{"", "Manager", 80000}
+	default:
+		panic("unsupported role")
+	}
 }
 
 func main() {
-  m := NewEmployee(Manager)
-  m.Name = "Sam"
-  fmt.Println(m)
+	m := NewEmployee(Manager)
+	m.Name = "Sam"
+	fmt.Println(m)
+
+	d := NewEmployee(Developer)
+	d.Name = "Samuel"
+	fmt.Println(d)
 }
---
+++ 9.Composite/README.md
@@ -0,0 +1,9 @@
+#### A mechanism for treating individual (scalar) objects and compositions of objects in a uniform manner.
+
+- Objects use other objects’ fields/methods through embedding   
+
+- Composition lets us make compound objects  
+ - E.g., a mathematical expression composed of simple expressions; or a shape group made of several different shapes  
+
+
+- Composite design pattern is used to treat both single (scalar) and composite objects uniformly i.e. Foo and []Foo have common APIs  
